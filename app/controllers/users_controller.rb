d65de5746adb1305fd8d99d838eff1e527617d75
Wills Blake <richwblake@gmail.com>
2020-05-17 17:55:10 -0400

    Added validations to User and defined user_params in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,15 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
     end
 
     def destroy
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+    # Enable bcrypt hashed password
     has_secure_password
+
+    # ActiveRecord associations
     has_many :subscriptions
     has_many :recipes, through: :subscriptions
+
+    # Validations on user creation
+    validates :name, :email, :password, presence: true
+    validates :email, uniqueness: true
 end
