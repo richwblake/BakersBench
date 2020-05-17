77a06869fa8fe3d1fbcec447f1495f52a0843afe
Wills Blake <richwblake@gmail.com>
2020-05-17 18:57:22 -0400

    Created root route and homepage. Created static controller and static view home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
     def index
     end
+
+    def show
+        @user = User.find_by(id: params[:id])
+    end
     
     def new
         @user = User.new
@@ -8,6 +12,11 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def destroy
@@ -17,5 +26,9 @@ class UsersController < ApplicationController
     
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def find_user
+        
     end
 end
