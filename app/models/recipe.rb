0fba0b0a8bf588fb7b2400f7fdca6dcec63448c9
Wills Blake <richwblake@gmail.com>
2020-05-17 15:52:42 -0400

    Created assocation between ingredient and recipe using recipe_ingredient JT

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,9 @@ class Recipe < ApplicationRecord
     has_many :subscriptions
     has_many :users, through: :subscriptions
 
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
+
     def owner
         User.find_by(id: self.creator_id)
     end
