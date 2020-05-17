12929d06d10fe1f60291b6ce74686d815a5fac23
Wills Blake <richwblake@gmail.com>
2020-05-17 15:42:44 -0400

    Created many to many associations between user and recipe. Adjusted subscription JT to accommodate assocations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+    has_many :subscriptions
+    has_many :users, through: :subscriptions
+
+    def owner
+        User.find_by(id: self.creator_id)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :subscriptions
+    has_many :recipes, through: :subscriptions
 end
