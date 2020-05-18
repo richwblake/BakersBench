e0613ef21329f8c342cda69c06d6660bd5aa9f41
Wills Blake <richwblake@gmail.com>
2020-05-17 23:45:59 -0400

    Created basic form partial for recipe. Added CRUD actions to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,25 @@
 class RecipesController < ApplicationController
+    before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+    before_action :logged_in?
+
+    def index
+        @recipes = Recipe.all
+    end
+    
     def show
     end
 
     def new
+        @recipe = Recipe.new
     end
 
     def create
+    end
+
+    def edit
+    end
+
+    def update
     end
 
     def destroy
@@ -15,5 +29,9 @@ class RecipesController < ApplicationController
 
     def recipe_params
         params.require(:recipe).permit(:title, :creator_id)
+    end
+
+    def find_recipe
+        redirect_to recipes_path, alert: "Recipe could not be found" unless @recipe = Recipe.find_by(id: params[:id])
     end
 end
