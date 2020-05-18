d2674616b31382d140e2afcd9d1b847e90191bdd
Wills Blake <richwblake@gmail.com>
2020-05-17 21:21:30 -0400

    Corrected session creation upon new account creation. Adjusted buttons to links to avoid unpredictable uris

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,10 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user), notice: "User created successfully"
         else
-            redirect_to new_user_path, alert: "Please try again, leaving no fields blank"
+            redirect_to new_user_path, alert: "Please try again"
         end
     end
 
