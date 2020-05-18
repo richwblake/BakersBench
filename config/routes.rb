40281e1ed32fb63d72c806d267dd26abe4d4ef54
Wills Blake <richwblake@gmail.com>
2020-05-17 22:21:01 -0400

    Added nested resources for recipes. User functionality preserved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'static#home'
-  
+
   resources :users
+  
+  resources :users, only: [:show] do 
+    resources :recipes
+  end
+
+  resources :recipes
+
   resources :sessions, only: [:new, :create, :destroy]
 end
